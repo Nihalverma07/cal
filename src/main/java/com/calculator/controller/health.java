3a145504a132a7b7cf8ea002d40edff04b16f49b
nihal <nihalverms32@gmail.com>
2025-04-12 12:31:51 +0530

    aff

--- src/main/java/com/calculator/controller/health.java
+++ src/main/java/com/calculator/controller/health.java
@@ -1,4 +1,15 @@
 package com.calculator.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
 public class health {
+
+    @GetMapping("message")
+    public String getmessage(){
+        return  "I am working fine :) ";
+    }
 }
